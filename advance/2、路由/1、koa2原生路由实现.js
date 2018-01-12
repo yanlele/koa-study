399d48221b73b90a1fa906b5746e52cd896d797a
yanlele <331393627@qq.com>
2018-01-13 00:27:21 +0800

    请求数据获取

--- advance/2、路由/1、koa2原生路由实现.js
+++ advance/2、路由/1、koa2原生路由实现.js
@@ -1,10 +1,63 @@
 /*
-const Koa=require('koa');
-const app=new Koa();
+ const Koa=require('koa');
+ const app=new Koa();
 
-app.use(async (ctx)=>{
-    let url=ctx.request.url;
-    ctx.body=url;
+ app.use(async (ctx)=>{
+ let url=ctx.request.url;
+ ctx.body=url;
+ });
+
+ app.listen(3000);*/
+
+
+const Koa = require('koa');
+const fs = require('fs');
+const app = new Koa();
+
+function render(page) {
+    return new Promise((resolve, reject) => {
+        let viewUrl = `./view/${page}`;
+        fs.readFile(viewUrl, 'binary', (err, data) => {
+            if (err) {
+                reject(err)
+            } else {
+                resolve(data)
+            }
+        })
+    })
+}
+
+async function route(url) {
+    let view = '404.html';
+    switch (url) {
+        case '/':
+            view = 'index.html';
+            break;
+
+        case '/index':
+            view = 'index.html';
+            break;
+
+        case '/todo':
+            view = 'todo.html';
+            break;
+
+        case '/404':
+            view = '404.html';
+            break;
+
+        default:
+            break;
+    }
+    let html = await render(view);
+    return html;
+}
+
+app.use(async (ctx) => {
+    let url = ctx.request.url;
+    let html = await route(url);
+    ctx.body = html;
 });
 
-app.listen(3000);*/
+app.listen(3000);
+console.log('port 3000');
