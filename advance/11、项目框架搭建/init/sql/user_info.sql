4be0120091d6b2706b79db356dd3320e64cbbf67
yanlele <331393627@qq.com>
2018-01-15 16:46:51 +0800

    项目框架搭建

--- advance/11、项目框架搭建/init/sql/user_info.sql
+++ advance/11、项目框架搭建/init/sql/user_info.sql
@@ -1,16 +1,14 @@
-create table IF NOT EXISTS  `user` (
+CREATE TABLE   IF NOT EXISTS  `user_info` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `email` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
   `name` varchar(255) DEFAULT NULL,
   `nick` varchar(255) DEFAULT NULL,
-  `detail_info` json DEFAULT NULL,
+  `detail_info` longtext DEFAULT NULL,
   `create_time` varchar(20) DEFAULT NULL,
   `modified_time` varchar(20) DEFAULT NULL,
   `level` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO `user` set email='1@example.com', password='123456';
-INSERT INTO `user` set email='2@example.com', password='123456';
-INSERT INTO `user` set email='3@example.com', password='123456';
+INSERT INTO `user_info` set name='admin001', email='admin001@example.com', password='123456';
